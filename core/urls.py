af4a9891302e03664813c347aaf3c2ac65fe9934
Akelvino <vinho892@gmail.com>
2026-01-01 12:25:06 +0300

    Finished settting the models and cloudinary

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='home')
+    path('', views.product_list, name='product-list'),
+    path('<slug:category_slug>/', views.product_list, name='product-list'),
+    path('product/<int:id>/<slug:slug>/', views.product_detail, name='product-detail')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
-def index(request):
-    return render(request, 'core/index.html')
+def product_list(request, category_slug=None):
+    category = None
+    products = Product.objects.filter(available = True)
+    categories = Category.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug = category_slug)
+        products = Product.filter(category=category)
+    
+    return render(request, 'core/list.html',{
+        'category':category,
+        'products':products,
+        'categories':categories
+    })
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'core/detail.html', {'product':product})
