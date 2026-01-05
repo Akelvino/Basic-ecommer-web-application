20ce465419bccbda6c5944406c927568b20fb68a
Akelvino <vinho892@gmail.com>
2026-01-04 23:47:42 -0800

    created the cart items model

--- core/models.py
+++ core/models.py
@@ -33,7 +33,15 @@ class Product(models.Model):
 
 
 class Cart(models.Model):
-    pass
+     created = models.DateTimeField(auto_now_add=True)
+
+     def get_total_price(self):
+         return sum(items.get_total_price() for item in self.items.all())
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name = 'items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='cart_items', on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+
+    def get_total_price(self):
+        return self.product.price * self.quantity
