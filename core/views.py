ad4d5c881747e0a5cfd3a4b8d0e12a9bae21c347
Akelvino <vinho892@gmail.com>
2026-01-05 23:57:42 -0800

    Added the cart feature

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category, Product, Cart, CartItem
+from django.views.decorators import require_POST
 
 # Create your views here.
 def product_list(request, category_slug=None):
@@ -20,3 +21,38 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'core/detail.html', {'product':product})
+
+@require_POST
+def cart_add(request, product_id):
+    cart_id = request.session.get('card_id')
+
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create()
+        else:
+            cart = Cart.objects.create()
+            request.session['cart_id'] = cart_id
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart_item , created = CartItem.objects.create(cart=cart, product=product)
+
+    if not created:
+        cart_item.quality +=1
+    
+    cart_item.save()
+
+    responce_data ={
+        'success':True,
+        'message':f"Added {product.name} to cart"
+    }
+
+def cart_detail(request):
+     cart_id = request.session.get('card_id')
+     cart = None
+     if cart_id:
+         cart = get_object_or_404(Cart, id=cart_id)
+         
+     return render(request, 'core/detail.html')
